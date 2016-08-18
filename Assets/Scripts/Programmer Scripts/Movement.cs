378896d3db6912a877a6854007109a6ffbc0a81e
Jaysen Griffiths <jaysen.griffith@ad.aie.edu>
2016-08-18 16:15:29 +0930

    Massive cleanup, clean project with movement and collision

--- Assets/Scripts/Programmer Scripts/Movement.cs
+++ Assets/Scripts/Programmer Scripts/Movement.cs
@@ -1,27 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Movement : MonoBehaviour {
 
+    public Camera cam;
     public Transform trans;
     private Rigidbody player;
     public float minBounds = 10;
     public float maxBounds = 20;
     int counter;
     public int moveTime = 20;
+    private MouseLook mouseLook;
     public float speed = 0.04f;
     // Use this for initialization
+
+    void Awake()
+    {
+        mouseLook = GetComponent<MouseLook>();
+    }
     void Start ()
     {
         float cameraAngle = Camera.main.transform.rotation.x;
         trans = gameObject.GetComponent<Transform>();
         player = gameObject.GetComponent<Rigidbody>();
         counter = 0;
-	}
+        mouseLook.Init(transform, cam.transform);
+    }
 	
 	// Update is called once per frame
 	private void Update ()
     {
+        RotateView();
         float cameraAngle = Camera.main.transform.eulerAngles.x;
 
         if (cameraAngle > minBounds && cameraAngle < maxBounds)
@@ -30,6 +40,24 @@ public class Movement : MonoBehaviour {
             counter = 0;
         if (counter > moveTime)
             isMoving();
+    }
+
+    private void RotateView()
+    {
+        //avoids the mouse looking if the game is effectively paused
+        if (Mathf.Abs(Time.timeScale) < float.Epsilon) return;
+
+        // get the rotation before it's changed
+        float oldYRotation = transform.eulerAngles.y;
+
+        mouseLook.LookRotation(trans, cam.transform);
+
+        //if (m_IsGrounded || advancedSettings.airControl)
+        //{
+        //    // Rotate the rigidbody velocity to match the new direction that the character is looking
+        //    Quaternion velRotation = Quaternion.AngleAxis(transform.eulerAngles.y - oldYRotation, Vector3.up);
+        //    m_RigidBody.velocity = velRotation * m_RigidBody.velocity;
+        //}
     }
 
     void isMoving()
