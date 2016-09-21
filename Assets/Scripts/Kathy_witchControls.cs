e84578d9d3522e4af3701cee8db22750cd310758
Kathleen Smart <kathleen.smart@ad.aie.edu>
2016-09-21 16:12:53 +0930

    New size level, animations added, optimisation for VR, FPS script

---
+++ Assets/Scripts/Kathy_witchControls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kathy_witchControls : MonoBehaviour
+{
+    static Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("isWaking");
+        }
+	}
+}
