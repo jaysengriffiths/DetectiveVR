7194121db06182d745d79549772347116df91b39
Kathleen Smart <kathleen.smart@ad.aie.edu>
2016-09-23 16:59:49 +0930

    Witch animations

--- Assets/Scripts/Kathy_witchControls.cs
+++ Assets/Scripts/Kathy_witchControls.cs
@@ -4,19 +4,41 @@ using System.Collections;
 public class Kathy_witchControls : MonoBehaviour
 {
     static Animator anim;
+    private bool talkingStillGoing = true;
 
 	// Use this for initialization
 	void Start ()
     {
         anim = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
+
     {
-	    if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetTrigger("isWaking");
         }
-	}
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            anim.SetTrigger("isStartingToTalk");
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            anim.SetTrigger("isPuttingOutHands");
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            anim.SetTrigger("isBringingBackHands");
+        }
+            
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            anim.SetTrigger("isListening");
+        }
+     }
 }
