2a86e6f8372da8d7fe666eaedda41ddcab4c6956
Kathleen Smart <kathleen.smart@ad.aie.edu>
2016-11-28 15:57:16 +1030

    Hiding script object

--- Assets/Scripts/Kathy/H1_HidingScript.cs
+++ Assets/Scripts/Kathy/H1_HidingScript.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class H1_HidingScript : MonoBehaviour
 {
-    public AudioClip L_02_Detective_2s;
-    public AudioClip C1_QuickHide_2;
-    public AudioClip C1A_Hurry_1;
-    public AudioClip C1B_GetChooksIn_1;
-    public AudioClip T1_RunAway_2;
+    public AudioClip H01_vBW_Hide_1;
+    public AudioClip H02_vDK_Hurry_3;
+    public AudioClip H03_vHK_ChooksIn_2;
+    public AudioClip H04_tF_Run_2;
+    public AudioClip H05_vBR_3;
 
-    public AudioClip henChased_2;
-    public AudioClip roosterChased_3;
+    public AudioClip HenHide3_2;
+    public AudioClip HenHide4_3;
 
     public AudioSource sourceForge;
     public AudioSource sourceHenHouse;
@@ -23,25 +23,27 @@ public class H1_HidingScript : MonoBehaviour
 
     IEnumerator H1_HidingSounds()
     {
-        sourceStables.PlayOneShot(L_02_Detective_2s);
         yield return new WaitForSeconds(2);
 
-        sourceStables.PlayOneShot(T1_RunAway_2);
+        sourceStables.PlayOneShot(H05_vBR_3);
         yield return new WaitForSeconds(2);
 
-        sourceForge.PlayOneShot(C1_QuickHide_2);
+        sourceStables.PlayOneShot(H01_vBW_Hide_1);
+        yield return new WaitForSeconds(1);
+
+        sourceHenHouse.PlayOneShot(H04_tF_Run_2);
+        yield return new WaitForSeconds(1.5f);
+
+        sourceForge.PlayOneShot(H02_vDK_Hurry_3);
+        yield return new WaitForSeconds(3);
+
+        sourceHenHouse.PlayOneShot(H03_vHK_ChooksIn_2);
         yield return new WaitForSeconds(2);
 
-        sourceHenHouse.PlayOneShot(C1A_Hurry_1);
+        sourceHenHouse.PlayOneShot(HenHide3_2);
         yield return new WaitForSeconds(1);
 
-        sourceHenHouse.PlayOneShot(C1B_GetChooksIn_1);
-        yield return new WaitForSeconds(1);
-
-        sourceHenHouse.PlayOneShot(henChased_2);
-        yield return new WaitForSeconds(1);
-
-        sourceHenHouse.PlayOneShot(roosterChased_3);
+        sourceHenHouse.PlayOneShot(HenHide4_3);
         yield return new WaitForSeconds(3);
     }
 }
